74d82a9493cb18d2da681e96548ea63120684464
Hiram <hiramtan@qq.com>
2016-03-15 10:14:31 +0800

    添加功能:1.log中添加时间;2.在屏幕上显示log内容;3.log写入到本地文件.

---
+++ unity/Assets/Example.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+public class Example : MonoBehaviour
+{
+    void Start()
+    {
+        MyDebug.EnabelOnScreen(true);
+        MyDebug.EnabelOnText(true);//写入到text文件中(persistent目录下)
+    }
+    int i = 0;
+    void Update()
+    {
+        if (i < 500)
+        {
+            i++;
+            MyDebug.Log(i);
+        }
+    }
+}
---
+++ visual/HiDebug/HiDebug/MyDebug.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public sealed class MyDebug : MonoBehaviour
+{
+    public static int itemCountOnScreen = 100;//拖拽区域包含多少条log信息
+    private static bool isLogOnScreen = false;
+    private static bool isLogOnConsole = false;
+    private static bool isLogOnText = false;
+    private static List<string> logOnScreenList = new List<string>();
+    private static string logOnScreen;
+    private static Vector2 scrollPosition;
+    private MyDebug() { }
+    public static void EnabelOnConsole(bool _isLogOnConsole)
+    {
+        isLogOnConsole = _isLogOnConsole;
+    }
+    public static void EnabelOnScreen(bool _isLogOnScreen)
+    {
+        isLogOnConsole = isLogOnScreen = _isLogOnScreen;
+        new GameObject("MyDebug").AddComponent<MyDebug>();
+        Application.logMessageReceived += (string _log, string _stackTrace, LogType _type) =>
+        {
+            UpdateScrollPosition();
+        };
+        Application.logMessageReceivedThreaded += (string _log, string _stackTrace, LogType _type) =>
+         {
+             UpdateScrollPosition();
+         };
+    }
+    public static void EnabelOnText(bool param)
+    {
+        isLogOnConsole = isLogOnText = param;
+    }
+    public static void Log(object _obj)
+    {
+        if (isLogOnConsole)
+        {
+            string log = string.Format(GetTime(), "Log", _obj.ToString());
+            Debug.Log(log);
+            if (isLogOnScreen)
+                OnScreen(log);
+            if (isLogOnText)
+                WriteLog(log);
+        }
+    }
+    public static void LogWarning(object _obj)
+    {
+        if (isLogOnConsole)
+        {
+            string log = string.Format(GetTime(), "Warning", _obj.ToString());
+            Debug.LogWarning(log);
+            if (isLogOnScreen)
+                OnScreen(log);
+            if (isLogOnText)
+                WriteLog(log);
+        }
+    }
+    public static void LogError(object _obj)
+    {
+        if (isLogOnConsole)
+        {
+            string log = string.Format(GetTime(), "Error", _obj.ToString());
+            Debug.LogError(log);
+            if (isLogOnScreen)
+                OnScreen(log);
+            if (isLogOnText)
+                WriteLog(log);
+        }
+    }
+    void OnGUI()
+    {
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(Screen.width), GUILayout.Height(Screen.height));
+        GUILayout.Label(logOnScreen);
+        GUILayout.EndScrollView();
+    }
+    static void UpdateScrollPosition()
+    {
+        scrollPosition = new Vector2(scrollPosition.x, scrollPosition.y + logOnScreenList.Count);
+    }
+    private static void OnScreen(string _log)
+    {
+        logOnScreenList.Add(_log);
+        if (logOnScreenList.Count > itemCountOnScreen)
+            logOnScreenList.RemoveAt(0);
+        logOnScreen = string.Empty;
+        foreach (string _s in logOnScreenList)
+            logOnScreen = string.IsNullOrEmpty(logOnScreen) ? logOnScreen = _s : logOnScreen = logOnScreen + "\n" + _s;//第一行不用换行
+    }
+    private static string GetTime()
+    {
+        DateTime time = DateTime.Now;
+        return time.ToString("yyyy.MM.dd HH:mm:ss") + "[{0}]: {1}";
+    }
+    private static void WriteLog(string param)
+    {
+        string path = Application.persistentDataPath + "/MyDebug.txt";
+        StreamWriter sw = File.AppendText(path);
+        sw.WriteLine(param);
+        sw.Close();
+    }
+}
